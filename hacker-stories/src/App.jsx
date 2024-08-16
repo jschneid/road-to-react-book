eeb2a6066890136c47fce12d9bdaa211506a6149
Jon Schneider <jon.schneider@gmail.com>
2024-08-16 13:10:49 -0400

    Section: React JSX

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -3,12 +3,30 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const title = 'React';
+const title = 'Road to React';
+
+const welcome = {
+  greeting: 'Salutations',
+  title: 'React',
+};
+
+const terranUnits = ['marine', 'firebat', 'vulture', 'seige tank', 'wraith'];
+
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
-      Hello, React!
+      <h1>{welcome.greeting}, {getTitle(welcome.title)}</h1>
+
+      <p>Some Terran units are:{' '}
+        {terranUnits.map((unit) => `${unit}, `)}
+      </p>
+
+      <label htmlFor="myLabel">Enter the value: </label>
+      <input type="text" id="myLabel" />
     </div>
   )
 }
