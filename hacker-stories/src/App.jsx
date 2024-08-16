93335c3f181a33bae897f7c6f60967ec9dd87e34
Jon Schneider <jon.schneider@gmail.com>
2024-08-16 13:54:59 -0400

    Section: Lists in React

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -5,28 +5,61 @@ import './App.css'
 
 const title = 'Road to React';
 
-const welcome = {
-  greeting: 'Salutations',
-  title: 'React',
-};
-
-const terranUnits = ['marine', 'firebat', 'vulture', 'seige tank', 'wraith'];
-
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: 'React', 
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  },
+  {
+    title: 'Aggro Magnet Games',
+    url: 'https://aggromagnetgames.com/',
+    author: 'Jon Schneider',
+    num_comments: 0,
+    points: 0,
+    objectID: 2
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>{welcome.greeting}, {getTitle(welcome.title)}</h1>
-
-      <p>Some Terran units are:{' '}
-        {terranUnits.map((unit) => `${unit}, `)}
-      </p>
-
-      <label htmlFor="myLabel">Enter the value: </label>
-      <input type="text" id="myLabel" />
+      Some titles are:
+      <ul>
+        {list.map((listItem) => 
+          <li key={listItem.objectID}>
+            <a href={listItem.url}>{listItem.title}</a>
+            <table>
+              <tbody>
+                <tr>
+                  <th>Author</th>
+                  <td>{listItem.author}</td>
+                </tr>
+                <tr>
+                  <th># Comments</th>
+                  <td>{listItem.num_comments}</td>
+                </tr>
+                <tr>
+                  <th>Points</th>
+                  <td>{listItem.points}</td>
+                </tr>
+              </tbody>
+            </table>
+          </li>
+          )
+        }
+      </ul>
     </div>
   )
 }
