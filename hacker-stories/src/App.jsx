cccdb7aecbd2907ce54aa421ddf9530a54a34642
Jon Schneider <jon.schneider@gmail.com>
2024-09-13 11:24:33 -0400

    Section: React Props

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -3,34 +3,35 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const list = [
-  {
-    title: 'React', 
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  },
-  {
-    title: 'Aggro Magnet Games',
-    url: 'https://aggromagnetgames.com/',
-    author: 'Jon Schneider',
-    num_comments: 0,
-    points: 0,
-    objectID: 2
-  }
-];
-
-const App = () => (
+const App = () => {
+  const stories = [
+    {
+      title: 'React', 
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    },
+    {
+      title: 'Aggro Magnet Games',
+      url: 'https://aggromagnetgames.com/',
+      author: 'Jon Schneider',
+      num_comments: 0,
+      points: 0,
+      objectID: 2
+    }
+  ];
+  
+  return (
   <div>
     <h1>My Hackier<sub>[sic]</sub> Stories</h1>
     <p>
@@ -42,9 +43,10 @@ const App = () => (
     <Search />
     <hr />
     Some titles are: 
-    <List />
+    <List list={stories} />
   </div>
-);
+  );
+};
 
 const Search = () => {
   const handleChange = (event) => {
@@ -60,31 +62,40 @@ const Search = () => {
   );
 };
 
-const List = () => (
+const List = (props) => (
   <ul>
-    {list.map((listItem) => 
-      <li key={listItem.objectID}>
-        <a href={listItem.url}>{listItem.title}</a>
-        <table>
-          <tbody>
-            <tr>
-              <th>Author</th>
-              <td>{listItem.author}</td>
-            </tr>
-            <tr>
-              <th># Comments</th>
-              <td>{listItem.num_comments}</td>
-            </tr>
-            <tr>
-              <th>Points</th>
-              <td>{listItem.points}</td>
-            </tr>
-          </tbody>
-        </table>
-      </li>
-      )
-    }
+    {props.list.map((listItem) => (
+      <Item key={listItem.objectID} listItem={listItem} />
+    ))}
   </ul>
 );
+
+const Item = (props) => (
+  <li>
+  <table>
+   <thead>
+      <tr>
+        <th colSpan={2}>
+          <a href={props.listItem.url}>{props.listItem.title}</a>
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th>Author</th>
+        <td>{props.listItem.author}</td>
+      </tr>
+      <tr>
+        <th># Comments</th>
+        <td>{props.listItem.num_comments}</td>
+      </tr>
+      <tr>
+        <th>Points</th>
+        <td>{props.listItem.points}</td>
+      </tr>
+    </tbody>
+  </table>
+</li>
+)
 
 export default App;
