d580ac9e395b0b38c3c9426d041d3dd65205c655
Jon Schneider <jon.schneider@gmail.com>
2025-03-28 11:23:41 -0400

    Section: React Advanced State: Refactor storiesReducer to use switch statement

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -47,12 +47,14 @@ const getAsyncStories = () => {
 };
 
 const storiesReducer = (state, action) => { 
-  if (action.type === 'SET_STORIES') {
-    return action.payload; 
-  } else if (action.type === 'REMOVE_STORY') {
-    return state.filter((story) => action.payload.objectID !== story.objectID);
+  switch(action.type) {
+    case 'SET_STORIES':
+      return action.payload;
+    case 'REMOVE_STORY':
+      return state.filter((story) => action.payload.objectID !== story.objectID);
+    default:
+      throw new Error();
   }
-  throw new Error();
 }
 
 const App = () => {
