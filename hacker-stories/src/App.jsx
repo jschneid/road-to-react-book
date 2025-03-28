8d23cbe7b57c800470a3b175abc067a2e188a33c
Jon Schneider <jon.schneider@gmail.com>
2025-03-28 15:23:13 -0400

    Section: Data Fetching with React
    Real data! Cool!

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -15,36 +15,7 @@ const useStorageState = (key, initialState) => {
   return [value, setValue];
 };
 
-const initialStories = [
-  {
-    title: 'React', 
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  },
-  {
-    title: 'Aggro Magnet Games',
-    url: 'https://aggromagnetgames.com/',
-    author: 'Jon Schneider',
-    num_comments: 0,
-    points: 0,
-    objectID: 2
-  }
-];
-
-const getAsyncStories = () => {
- return new Promise((resolve) => setTimeout(() => resolve({ data: { stories: initialStories } }), 1500));
-};
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const storiesReducer = (state, action) => { 
   switch (action.type) {
@@ -62,7 +33,6 @@ const storiesReducer = (state, action) => {
 }
 
 const App = () => {
-
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
   const [stories, dispatchStories] = useReducer(storiesReducer, {
     data: [], isLoading: false, errorOccurred: false
@@ -71,10 +41,12 @@ const App = () => {
   useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories().then((result) => {
+    fetch(`${API_ENDPOINT}react`)
+    .then((response) => response.json())
+    .then((result) => {
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS', 
-        payload: result.data.stories
+        payload: result.hits
       });
     })
     .catch(() => 
@@ -91,7 +63,7 @@ const App = () => {
   };
 
   const storiesMatchingSearchTerm = stories.data.filter((story) => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
-  
+
   const handleRemoveStory = (story) => {
     dispatchStories({
       type: 'REMOVE_STORY',
